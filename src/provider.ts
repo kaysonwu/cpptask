56191ebbf79d45154b203fb2870df27d01b4d466
kaysonwu <772663139@qq.com>
2020-08-21 21:44:58 +0800

    update

--- src/provider.ts
+++ src/provider.ts
@@ -1,40 +1,85 @@
+import { parse, join } from 'path';
+import { mkdirSync, symlinkSync } from 'fs';
+import type { ExecOptions } from 'child_process';
 import { TaskProvider, Task, TaskScope, CustomExecution } from 'vscode';
 import * as globby from 'globby';
 import { CppTaskDefinition } from './interface';
-import { mergeDefinition, normalizePatterns, relatives } from './utils';
+import { mergeDefinition, normalizePatterns, relatives, slash } from './utils';
 import Terminal from './terminal';
 
 class CppTaskProvider implements TaskProvider {
 
-  private static async parseDefinition(definition: CppTaskDefinition): Promise<CppTaskDefinition> {
-    const { cwd } = definition.options || {};
-    let { sources, includePath, libraryPath } = definition;
-
-    sources = await globby(
-      normalizePatterns(sources),
+  private static async parseSources(patterns: string[], cwd?: string) {
+    const files = await globby(
+      normalizePatterns(patterns),
       { cwd, expandDirectories: false, onlyFiles: true },
     );
-
+    
     // Mainly used to shorten command parameters
     if (cwd) {
-      sources = relatives(cwd, sources);
+      return relatives(cwd, files);
     }
+
+    return files;
+  }
+
+  private static async parseLibraies(patterns: string[], output: string, cwd?: string) {
+    const files = await globby(
+      normalizePatterns(patterns),
+      { cwd, expandDirectories: false, onlyFiles: true },
+    );
+    
+    const directories: string[] = [];
+    const names: string[] = [];
+
+    files.forEach(file => {
+      const { dir, base, name } = parse(file);
+      names.push(name);
+
+      try {
+        symlinkSync(file, join(output, base));
+      } catch { }
+
+      if (!directories.includes(dir)) {
+        directories.push(dir);
+      }
+    }); 
+    
+    return [directories, names];
+  }
+
+  private static async parseDefinition({
+    compilerPath, 
+    compilerArgs, 
+    sources,
+    output,
+    includePath,
+    libraryPath, 
+    options: { cwd, env } = {},
+  }: CppTaskDefinition): Promise<[string, string[], ExecOptions]> {
+    const { dir } = parse(output);
+    const options = { cwd, env: { ...env } };
+    const args = await CppTaskProvider.parseSources(sources, cwd);
+ 
+    // Always try to create the output directory.
+    mkdirSync(dir, { recursive: true });
     
     if (includePath) {
-      includePath = await globby(
+      options.env.CPATH = (await globby(
         normalizePatterns(includePath), 
         { cwd, expandDirectories: false, onlyDirectories: true },
-      );
+      )).join(';');
     }
 
     if (libraryPath) {
-      libraryPath = await globby(
-        normalizePatterns(libraryPath),
-        { cwd, expandDirectories: false, onlyFiles: true },
-      );
+      const [directories, names] = await CppTaskProvider.parseLibraies(libraryPath, dir, cwd);
+      options.env.LIBRARY_PATH = directories.join(';');
+      names.forEach(name => args.unshift('-l', name));
     }
 
-    return { ...definition, sources, includePath, libraryPath };
+    args.unshift(...compilerArgs, '-o', slash(output));
+
+    return [compilerPath, args, options];
   }
 
   public provideTasks() {
@@ -52,7 +97,7 @@ class CppTaskProvider implements TaskProvider {
         'cpp', 
         'cpp',
         // @ts-ignore
-        new CustomExecution(async definition => new Terminal(await CppTaskProvider.parseDefinition(definition))),
+        new CustomExecution(async definition => new Terminal(...(await CppTaskProvider.parseDefinition(definition)))),
       );
     }
 
